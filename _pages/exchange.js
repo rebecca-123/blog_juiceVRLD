c4cbdeaf978b3d021f85a9b37abd6cb63a6fc2c3
vaishavijay <vaishavijay@users.noreply.github.com>
2022-10-28 17:23:34 +0000

    deploy: 19532bbdbcc3e8c32cf76527a21f88db31337f40

--- _pages/exchange.js
+++ _pages/exchange.js
@@ -1,1 +1,10 @@
-let btn = document.getElementById("btn")
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click",function() {
+    let naria = document.getElementById("naria").value;
+    let ringgit = document.getElementById("ringgit");
+    let dollar = document.getElementById("dollar");
+    ringgit.value = naria / 100;
+    dollar.value = naria / 413
+});
